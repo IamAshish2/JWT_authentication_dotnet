a4aad6ef7e39cbdf68073ee647a040b90a9e07ca
IamAshish2 <karkiaashish9899@gmail.com>
2024-10-17 19:14:54 +0545

    add authentication service

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace JWT_AUTHENTICATION.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : Controller
+    {
+
+        [Authorize]
+        [HttpGet("data")]
+        public IActionResult Index()
+        {
+            // decoding claims
+            string id = HttpContext.User.FindFirstValue("id")!;
+            string email = HttpContext.User.FindFirstValue(ClaimTypes.Email)!;
+            string username = HttpContext.User.FindFirstValue(ClaimTypes.Name)!;
+            return Ok(new { id, email, username });
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,21 +6,20 @@ using JWT_AUTHENTICATION.Services.UserRepositories;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
+
+// configuring dbContext with appDbcontext to get the connection string for database from appsettings.json
+builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 // Access the configuration object
 var configuration = builder.Configuration;
 
 // Add services to the container.
 builder.Services.AddControllers();
-
-// Bind AuthenticationConfiguration from appsettings.json
-AuthenticationConfiguration authenticationConfiguration = new AuthenticationConfiguration();
-configuration.Bind("Jwt", authenticationConfiguration);
-builder.Services.AddSingleton(authenticationConfiguration);
-
 builder.Services.AddScoped<IPasswordHasher,BcryptPasswordHasher>();
 builder.Services.AddScoped<IUserRepository, UserRepository>();
 builder.Services.AddScoped<AccessTokenGenerator>();
@@ -28,6 +27,55 @@ builder.Services.AddEndpointsApiExplorer();
 
 builder.Services.AddSwaggerGen();
 
+// check for bearer token usuage
+builder.Services.AddSwaggerGen(opt =>
+{
+    opt.SwaggerDoc("v1", new OpenApiInfo { Title = "MyAPI", Version = "v1" });
+    opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        BearerFormat = "JWT",
+        Scheme = "bearer"
+    });
+
+    opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type=ReferenceType.SecurityScheme,
+                    Id="Bearer"
+                }
+            },
+            new string[]{}
+        }
+    });
+});
+
+
+// Bind AuthenticationConfiguration from appsettings.json
+AuthenticationConfiguration authenticationConfiguration = new AuthenticationConfiguration();
+configuration.Bind("Jwt", authenticationConfiguration);
+builder.Services.AddSingleton(authenticationConfiguration);
+
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters()
+    {
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(authenticationConfiguration.AccessTokenKey)),
+        ValidIssuer = authenticationConfiguration.Issuer,
+        ValidAudience = authenticationConfiguration.Audience,
+        ValidateIssuerSigningKey = true,
+        ValidateAudience = true,
+        ValidateIssuer = true,
+        ClockSkew = TimeSpan.Zero
+    };
+});
 
 builder.Services.AddAuthorization(options =>
 {
@@ -36,9 +84,6 @@ builder.Services.AddAuthorization(options =>
             .RequireAuthenticatedUser() 
             .Build();
 });
-
-// configuring dbContext with appDbcontext to get the connection string for database from appsettings.json
-builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 
 var app = builder.Build();
@@ -52,6 +97,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
