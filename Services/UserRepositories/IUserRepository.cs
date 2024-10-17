1be5e6c9f29da571a7ec1b2539f023a3ac5026ec
IamAshish2 <karkiaashish9899@gmail.com>
2024-10-17 18:36:16 +0545

    services handler's for user actions

---
+++ Services/UserRepositories/IUserRepository.cs
@@ -0,0 +1,12 @@
+﻿using JWT_AUTHENTICATION.Models;
+
+namespace JWT_AUTHENTICATION.Services.UserRepositories
+{
+    public interface IUserRepository
+    {
+        Task<User> GetByEmail(string email);
+        Task<User> GetByUserName(string userName);
+        Task<User> CreateUser(User user);
+        //Task<User> UserExists(string userName);
+    }
+}
---
+++ Services/UserRepositories/UserRepository.cs
@@ -0,0 +1,35 @@
+﻿using JWT_AUTHENTICATION.Data;
+using JWT_AUTHENTICATION.Models;
+
+namespace JWT_AUTHENTICATION.Services.UserRepositories
+{
+    public class UserRepository : IUserRepository
+    {
+
+        private readonly AppDbContext _context;
+
+        public UserRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task<User> CreateUser(User user)
+        {
+            user.Id = Guid.NewGuid();
+            _context.Users.Add(user);
+            _context.SaveChanges(); 
+            return Task.FromResult(user);
+        }
+
+        // check this method for ! non-nullable references later->>>>>
+        public Task<User> GetByEmail(string email)
+        {
+            return Task.FromResult(_context.Users.Where(u => u.Email == email).FirstOrDefault())!;
+        }
+
+        public Task<User> GetByUserName(string userName)
+        {
+            return Task.FromResult(_context.Users.Where(u => u.UserName == userName).FirstOrDefault())!;
+        }
+    }
+}
