6b6fdaae7b981d42857f0adcd0d067a7bc4e028a
IamAshish2 <karkiaashish9899@gmail.com>
2024-10-17 22:23:13 +0545

    validate refresh tokens

---
+++ Services/TokenValidator/RefreshTokenValidator.cs
@@ -0,0 +1,43 @@
+﻿using JWT_AUTHENTICATION.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace JWT_AUTHENTICATION.Services.TokenValidator
+{
+    public class RefreshTokenValidator
+    {
+
+        private AuthenticationConfiguration _configuration;
+
+        public RefreshTokenValidator(AuthenticationConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
+        public bool Validate(string refreshToken)
+        {
+             
+            TokenValidationParameters validationParameters = new TokenValidationParameters
+            {
+                IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.RefreshTokenKey)),
+                ValidIssuer = _configuration.Issuer,
+                ValidAudience = _configuration.Audience,
+                ValidateIssuerSigningKey = true,
+                ValidateAudience = true,
+                ValidateIssuer = true,
+                ClockSkew = TimeSpan.Zero
+            };
+            JwtSecurityTokenHandler tokenHandler = new JwtSecurityTokenHandler();
+            try
+            {
+                tokenHandler.ValidateToken(refreshToken, validationParameters, out SecurityToken validatedToken);
+                return true;
+
+            }catch (Exception ex)
+            {
+                return false;
+            }
+        }
+    }
+}
